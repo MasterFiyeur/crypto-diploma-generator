ffee11fe06a47c45af2a87714aa73c1f2d2f1f62
Théo Julien <tjulien@temma.care>
2022-10-10 16:53:47 +0200

    [CHG] tab space

--- webapp/static/js/login.js
+++ webapp/static/js/login.js
@@ -1,21 +1,21 @@
 async function login(){
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    console.log(email, password);
-    fetch("/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    }).then(response => {
-        if (response.status === 200){
-            window.location.href = "/home";
-        } else {
-            alert("Login failed");
-        }
-    });
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value;
+	console.log(email, password);
+	fetch("/api/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			email: email,
+			password: password
+		})
+	}).then(response => {
+		if (response.status === 200){
+			window.location.href = "/home";
+		} else {
+			alert("Login failed");
+		}
+	});
 }
