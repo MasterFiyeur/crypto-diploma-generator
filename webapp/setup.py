a2b4d19075f86202c02781ed81ff619c1f946292
Lucas <lucas.teste@sfr.fr>
2022-10-10 16:27:30 +0200

    login

--- webapp/setup.py
+++ webapp/setup.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Diploma Generator'
+    return render_template('index.html')
 
+@app.route('/login')
+def hello():
+    return render_template('login.html')
